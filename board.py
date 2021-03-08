c68518154215bc1cbe43597e5d663252533305f0
git config --global user.name kevingit config --global user.name <kayvinhnguyen@gmail.com>
2021-03-07 18:21:58 -0800

    added validity check function

--- board.py
+++ board.py
@@ -10,13 +10,41 @@ board = [
     [0,4,9,2,0,6,0,0,7]
 ]
 
+def valid(board1, num, pos):
+    """
+    Checks row and column for valid values
+    returns True unless values are not valid
+    """
+    # check row
+    for i in range(len(board1[0])):
+        if board1[i][pos[1]] == num and pos[1] != i:        # check each element in row 
+            return False
+
+    # check column
+    for i in range(len(board1[0])):
+        if board1[i][pos[1]] == num and pos[0] != i:  
+            return False
+
+    # check which box we're in
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if board1[i][j] == num and (i,j) != pos:
+                return False
+
+    return True
+
 def printBoard(board1):
+    # create division line for every 3rd row
     for i in range(len(board1)):
-        if i % 3 == 0 and i != 0:           # creates division line for every 3rd row
+        if i % 3 == 0 and i != 0:
             print("-----------------------")
-    
+
+        # create division line for every 3rd column 
         for j in range(len(board1[0])):
-            if j % 3 == 0 and j != 0:       # creates division line for every 3rd column
+            if j % 3 == 0 and j != 0:
                 print(" | ", end = "")
 
             if j == 8:
@@ -25,6 +53,11 @@ def printBoard(board1):
                 print(str(board1[i][j]) + " ", end = "")
 
 def empty(board1):
+    """
+    finds all empty 0's on the board
+
+    :return: tuple of position as (row,col)
+    """
     for i in range(len(board1)):
         for j in range(len(board1[0])):
             if board1[i][j] == 0:
